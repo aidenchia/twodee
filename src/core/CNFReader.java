748c6548ffbbadda94ccb7232d9f575e2f31bd91
Aiden Chia <aidenchia95@gmail.com>
2018-10-15 21:54:13 +0800

    CNF Reader

--- src/core/CNFReader.java
+++ src/core/CNFReader.java
@@ -7,9 +7,9 @@ public class CNFReader
 	private static ArrayList<String> stuff = new ArrayList<String>();
 	private static String line;
 	
-	public static void ReadFile() throws Exception
+	public static void main(String[] args) throws Exception
 	{
-		File file = new File("/Users/aidenchia/Documents/@SUTD/Term\\ 4/Java/twodee/src/sampleCNF/largeSat.cnf");
+		File file = new File("/Users/aidenchia/Documents/@SUTD/Term 4/Java/twodee/src/sampleCNF/largeSat.cnf");
 
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		
